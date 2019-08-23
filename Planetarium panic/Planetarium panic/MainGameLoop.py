875e5209b207457565e99f59f7a7526a61ac48a2
VikramVenkatesh96 <46155682+VikramVenkatesh96@users.noreply.github.com>
2019-08-23 12:59:44 -0600

    Sample template for maingameloop

---
+++ Planetarium panic/Planetarium panic/MainGameLoop.py
@@ -0,0 +1,37 @@
+#Sample template for the main game loop
+
+#All the imports required
+#import Loader
+#import Physics
+
+#All Globals here
+#FPS = 30
+
+#Start function
+#def Start():
+    
+#    Initialize variables
+#    count = 0
+
+#    Initializing Elements
+#    Player= Element(Loader(),Physics())
+    
+#    Function to generate the level
+#    GenerateLevel()
+
+#Per-Frame update function
+#def Update():
+
+#    Core mechanics and check Win/fail states
+#    render and update
+
+
+#Main Function
+#if__name__ == "__main__":
+    
+#    Initialize stuff here
+#    Start()
+
+#    while True:
+#        Per-Frame updates
+#        Update()
