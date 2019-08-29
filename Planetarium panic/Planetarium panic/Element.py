d60f0fbb25611bbd54ccf1c577106c8f945cdb19
ManasR4A <54380455+ManasR4A@users.noreply.github.com>
2019-08-28 22:53:55 -0600

    Update Element.py

--- Planetarium panic/Planetarium panic/Element.py
+++ Planetarium panic/Planetarium panic/Element.py
@@ -1,20 +1,34 @@
-import pygame, os, sys, Loader
+import pygame, sys, os
+from pygame.math import Vector2
 
 
-class element:
+class Entity(pygame.sprite.Sprite):
 
-    level = 0  # VARIABLE TO DEFINE THE LEVEL AT WHICH THE ELEMENT IS
+    def __init__(self, pos, tex, offset, rotAngle, scale): # rotAngle +ve for clockwise rotation, -ve for anti-clockwise rotation
+        super().__init__()
+        self.image = pygame.image.load(os.getcwd()+'\\images\\'+tex)
+        self.image = pygame.transform.scale(self.image, (scale))
+        # A reference to the original image to preserve the quality.
+        self.orig_image = self.image
+        self.rect = self.image.get_rect(center=pos)
+        self.mask = pygame.mask.from_surface(self.image)
+        self.pos = Vector2(pos)  # The original center position/pivot point.
+        self.offset = Vector2(offset)  # We shift the sprite offset px to the right.
+        self.angle = 0
+        self.rotAngle = rotAngle
 
-    def __init__(self, texObject):
-        self.elementObject = Loader.loaderObjects(texObject)
+    def updateDir(self, dir):
+        self.rotAngle = dir
 
-    def genereate(level):
+    def update(self):
+        self.angle = self.angle + self.rotAngle
+        self.rotate()
 
-        print("New Element is Generated from the given parameters ")
-
-    def destroy():
-        #DESTROYS THE SELFOBJECT WHEN CALLED 
-        print("Destroyed")
-
-
-
+    def rotate(self):
+        """Rotate the image of the sprite around a pivot point."""
+        # Rotate the image.
+        self.image = pygame.transform.rotozoom(self.orig_image, -self.angle, 1)
+        # Rotate the offset vector.
+        offset_rotated = self.offset.rotate(self.angle)
+        # Create a new rect with the center of the sprite + the offset.
+        self.rect = self.image.get_rect(center=self.pos+offset_rotated)
